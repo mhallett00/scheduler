c8e9ce21209595b0a2fd95458ce128aa5e085413
Michael Hallett <m.hallett@gmail.com>
2020-06-10 19:43:02 -0400

    Integrated appointment views into app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DayList from "./DayList"
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
@@ -21,9 +22,79 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Brian Lines",
+      interviewer: {
+        id: 2,
+        name: "Gareth Nichols",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "5pm",
+    interview: {
+      student: "John Jackson",
+      interviewer: {
+        id: 3,
+        name: "Tery Canter",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "6pm",
+    interview: {
+      student: "Sarah Sequira",
+      interviewer: {
+        id: 4,
+        name: "Danny Oberton",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: "last",
+    time: "7pm",
+  }
+];
+
 export default function Application(props) {
   const [ day , setDay ] = useState("Monday");
+  // const { appointments } = props
+  console.log(appointments)
+  const parsedAppointments = appointments.map(appointment => {
   
+    return (
+      <Appointment 
+        key={appointment.id} 
+        {...appointment}
+      />
+    )
+  })
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,7 +118,7 @@ export default function Application(props) {
         
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {parsedAppointments}
       </section>
     </main>
   );
