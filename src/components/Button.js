d69f182f296ec3da45ee699fc57e297c93e1e500
Michael Hallett <m.hallett@gmail.com>
2020-06-10 13:10:15 -0400

    General button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,24 @@
 import React from "react";
+import ClassNames from  "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   const buttonClass = ClassNames (
+      "button",
+      {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger   
+      }
+   )
+  
+   return (
+      <button
+         className={buttonClass} 
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
+ }
