b7b33410c7516c6d392e1e5f7c7b16e48580ac73
Michael Hallett <m.hallett@gmail.com>
2020-06-11 21:29:31 -0400

    Added getInterview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,5 @@
-import React from "react";
-
-export function getAppointmentsForDay(state, day) {
-  const findNames = state.days && state.days.find(dayObj => dayObj.name === day); 
+export function getAppointmentsForDay(state, day) {  
+  const findNames = state.days && state.days.find(dayObj => dayObj.name === day);
 
   if (!findNames) return [];
   
@@ -9,5 +7,27 @@ export function getAppointmentsForDay(state, day) {
     return state.appointments[appointmentId];
 
   })
+
   return apptArr
-}
+};
+
+export function getInterview (state, interview) {
+    
+
+  if (interview) {
+    const interviewerId = interview.interviewer;
+
+    let output = {
+        student: interview.student,
+        interviewer: {
+          id: interviewerId,
+          name: state.interviewers[interviewerId].name,
+          avatar: state.interviewers[interviewerId].avatar
+      }
+    }
+
+    return output
+  }
+
+  return null
+};
