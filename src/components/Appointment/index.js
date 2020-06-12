71e44c6688fff6d0172710c37a4a4b3100178ee6
Michael Hallett <m.hallett@gmail.com>
2020-06-12 16:00:02 -0400

    Added handling to create and cancel a new appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,40 @@ import "./styles.scss";
 import Header from "./Header"
 import Empty from "./Empty"
 import Show from "./Show"
+import useVisualMode from "../../hooks/useVisualMode";
+import Form from "./Form";
+
 
 export default function Appointment (props) {
   const {interview } = props;
+  
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  const onAdd = () => transition(CREATE); 
+  const onCancel = () => back();
 
   return (
     <article className="appointment">
      {/* {props.time} */}
       <Header time={props.time} />
-      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      { mode === CREATE && (
+        <Form
+        interviewers={[]}
+        onSave={props.onSave}
+        onCancel={onCancel}
+        />
+      )}
     </article>
   )
 }
