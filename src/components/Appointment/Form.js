c7076297bdf503ef85392411b764ca4fa854e599
Michael Hallett <m.hallett@gmail.com>
2020-06-13 14:48:46 -0400

    a space

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -16,7 +16,7 @@ export default function Form(props) {
     props.onCancel(reset());
 
   }
-
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
