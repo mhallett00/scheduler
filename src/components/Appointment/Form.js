b5562cf20b7e70c2e32d8da7966b446c4e980fdd
Michael Hallett <m.hallett@gmail.com>
2020-06-15 20:07:41 -0400

    Added a requirement for student name when booking interviews

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import Button from "../Button";
 export default function Form(props) {
   const [ name, setName ] = useState(props.name || "");
   const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+  const [ error, setError ] = useState("");
 
   const reset = () => {
     setName("");
@@ -15,6 +16,16 @@ export default function Form(props) {
   const cancel = () => {
     props.onCancel(reset());
 
+  }
+
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    
+    setError("");
+    props.onSave(name, interviewer);
   }
   
   return (
@@ -28,14 +39,16 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={ e => setName(e.target.value) }
-          />
+            data-testid="student-name-input"
+            />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={e => cancel()}>Cancel</Button>
-          <Button confirm onClick={e => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={e => validate()}>Save</Button>
         </section>
       </section>
     </main>
