3b31d28a1570bc27fc5e066487555a73fdd828e8
Michael Hallett <m.hallett@gmail.com>
2020-06-17 20:35:15 -0400

    A space

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
 export function getAppointmentsForDay(state, day) {  
   const findNames = state.days && state.days.find(dayObj => dayObj.name === day);
-
+  
   if (!findNames) return [];
   
   const apptArr = findNames.appointments.map(appointmentId => {
